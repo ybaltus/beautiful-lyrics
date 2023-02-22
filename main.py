f65a0a2da023ea26cbb7b67cf2d0a5ced2ebf87a
Yannis Baltus <yannis.baltus@outlook.com>
2023-02-22 21:44:27 +0100

    Get all words and the top

--- main.py
+++ main.py
@@ -7,5 +7,8 @@
 import utils.api as api
 import pprint
 
-all_lyrics_links = api.get_all_lyrics_urls()
-pprint.pprint(all_lyrics_links)
+# Get the words for all songs
+all_words = api.get_all_words()
+# Extract the top 10
+top_10_words = api.get_top_words(all_words, 10)
+pprint.pprint(top_10_words)
--- utils/api.py
+++ utils/api.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+import collections
 
 def get_all_lyrics_urls():
     try:
@@ -18,11 +20,45 @@ def get_all_lyrics_urls():
         return list_urls
     except requests.exceptions.ConnectionError:
         print("L'URL n'est pas valide ou est incorrect.")
-        return
+        return []
     except:
         print("Une erreur est survenue")
-        return
+        return []
 
 
-def extract_lyrics(url_lyrics : str):
-    print("OK")
+def extract_lyrics(url_lyrics : str, word_length = 2):
+    r = requests.get(url_lyrics)
+    print(url_lyrics)
+
+    if r.status_code != 200:
+        print('Page impossible à récupérer.')
+        return []
+    # Create soup object
+    page_html_soup = BeautifulSoup(r.text, 'html.parser')
+
+    # Extract the words
+    sentences = page_html_soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-6 YYrds")
+    lyrics_words = []
+    if sentences:
+        for sentence in sentences.stripped_strings:
+            sentence_words = [word.strip("[()]\",.").lower() for word in sentence.split() if len(word) > 2]
+            lyrics_words.extend(sentence_words)
+   
+    return lyrics_words
+
+def get_top_words(words: list, top_count=10):
+    # Count and extract the top 10
+    counter = collections.Counter(words)
+    return counter.most_common(10)
+
+def get_all_words():
+    all_lyrics_urls = get_all_lyrics_urls()
+    print("Nombre d'urls = " + str(len(all_lyrics_urls)))
+    if len(all_lyrics_urls) > 0:
+        all_words = []
+        for lyrics_url in all_lyrics_urls:
+            lyrics = extract_lyrics(lyrics_url)
+            all_words.extend(lyrics)
+        return all_words
+    else:
+        return []
